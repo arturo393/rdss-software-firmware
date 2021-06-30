815d62592cb9a9e7785407b0df4ff50b00984a2d
Yery Tannus <ytannus@itaum.com>
2021-06-30 17:11:09 -0400

    +socketIO Middleware

--- apps/frontend/src/redux/actions/main.js
+++ apps/frontend/src/redux/actions/main.js
@@ -1,8 +1,6 @@
 import * as t from "../types"
 
 export const setName = (name) => (dispatch) => {
-  console.log("ACTION: SET NAME")
-
   dispatch({
     type: t.SET_NAME,
     payload: name,
@@ -10,7 +8,6 @@ export const setName = (name) => (dispatch) => {
 }
 
 export const setNameEvent = (name) => (dispatch) => {
-  console.log("ACTION: SET NAME EVENT")
   dispatch({
     event: "set_name_event",
     handle: (data) =>
--- apps/frontend/src/redux/reducers/main.js
+++ apps/frontend/src/redux/reducers/main.js
@@ -8,13 +8,6 @@ const main = (
 ) => {
   switch (action.type) {
     case t.SET_NAME:
-      console.log("SET NAME REDUCER")
-      return {
-        ...state,
-        name: action.payload,
-      }
-    case t.SET_NAME_EVENT:
-      console.log("SET NAME EVENT REDUCER")
       return {
         ...state,
         name: action.payload,
