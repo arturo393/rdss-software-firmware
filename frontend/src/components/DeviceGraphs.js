087b710286b7f1ad72953ea87778250e8fe3bb5a
Hossman <hossman.escobar@usach.cl>
2021-07-26 17:15:08 -0400

    +Graphs

--- frontend/src/components/DeviceGraphs.js
+++ frontend/src/components/DeviceGraphs.js
@@ -1,20 +1,26 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 //import hammer from 'hammerjs'
-import { Line } from 'react-chartjs-2';
-//import zoomPlugin from 'chartjs-plugin-zoom';
+import {Chart, Line } from 'react-chartjs-2'
+import * as Zoom from "chartjs-plugin-zoom";
 import { connect } from "react-redux"
-import { Button } from 'react-bootstrap';
-import { convertISODateToTimeFormat  } from '../lib/Utils';
+import { Button } from 'react-bootstrap'
+import { convertISODateToTimeFormat, initConfig, setGraficoData, addGraficoData} from '../lib/Utils'
 
 const DeviceGraphs = (props) => {
 
   const { deviceId, monitorData } = props
   const [device, setDevice] = useState({})
-  const [grafico, setGrafico] = useState({})
-  
-  // Va a buscar los datos a la API
-  // Refresca los gráficos desde monitorData del redux
+  const [graficoVoltaje, setGraficoVoltaje] = useState(initConfig('rgba(75,192,192,1)','rgba(0,0,0,1)', 'Voltaje'))
+  const [graficoCurrent, setGraficoCurrent] = useState(initConfig('rgba(75,192,192,1)','rgba(0,0,0,1)', 'Current'))
+  const [graficoPower, setGraficoPower] = useState(initConfig('rgba(75,192,192,1)','rgba(0,0,0,1)', 'Power'))
+
+
+  useEffect(() => {
+    //Chart.register(Zoom);
+  }, []);
+
+ /* Actualizacion por BD*/
   useEffect(() => {
 
     if (deviceId > 0) {
@@ -23,35 +29,34 @@ const DeviceGraphs = (props) => {
         device = res.data.find(data => data.id == deviceId)
 
         setDevice(device)
-        let labelGraph = []
-        let dataGraph = []
+        let labelGraphVoltaje = []
+        let labelGraphCurrent = []
+        let labelGraphPower = []
+
+        let dataGraphVoltaje = []
+        let dataGraphCurrent = []
+        let dataGraphPower = []
 
         device.rtData?.map((obj) => {
-          labelGraph.push(convertISODateToTimeFormat(obj.sampleTime))
+          labelGraphVoltaje.push(convertISODateToTimeFormat(obj.sampleTime))
+          labelGraphCurrent.push(convertISODateToTimeFormat(obj.sampleTime))
+          labelGraphPower.push(convertISODateToTimeFormat(obj.sampleTime))
+
+          dataGraphCurrent.push(obj.current)
+          dataGraphPower.push(obj.power)
+          dataGraphVoltaje.push(obj.voltaje)
         })
 
-        device.rtData?.map((obj) => {
-          dataGraph.push(obj.voltaje)
-        })
+        setGraficoVoltaje(setGraficoData({ ...graficoVoltaje },labelGraphVoltaje,dataGraphVoltaje))
+        setGraficoCurrent(setGraficoData({ ...graficoCurrent },labelGraphCurrent,dataGraphCurrent))
+        setGraficoPower(setGraficoData({ ...graficoPower },labelGraphPower,dataGraphPower))
 
-        setGrafico({
-          labels: labelGraph,
-          datasets: [
-            {
-              label: 'Voltaje',
-              fill: false,
-              lineTension: 0.5,
-              backgroundColor: 'rgba(75,192,192,1)',
-              borderColor: 'rgba(0,0,0,1)',
-              borderWidth: 2,
-              data: dataGraph
-            }
-          ]
-        })
       })
     }
   }, [deviceId])
 
+  
+  /* Actualizacion por Sockect */
   useEffect(() => {
 
     if (deviceId > 0) {
@@ -63,6 +68,7 @@ const DeviceGraphs = (props) => {
         }
       })
 
+      /*INICIO DUMMY */
       const rtData = {
         voltaje: 12,
         current: 50,
@@ -71,16 +77,17 @@ const DeviceGraphs = (props) => {
         power: 100,
         sampleTime: "2021-07-27 14:29:22"
       }
-
       dataDevice.rtData = rtData
-      let stateGrafico = { ...grafico }
-      stateGrafico.labels.push(dataDevice.rtData.sampleTime)
-      stateGrafico.datasets[0].data.push(dataDevice.rtData.voltaje)
-      setGrafico(stateGrafico)
+      /*FIN  DUMMY */
+
+      setGraficoVoltaje(addGraficoData({ ...graficoVoltaje },dataDevice.rtData.sampleTime,dataDevice.rtData.voltaje))
+      setGraficoCurrent(addGraficoData({ ...graficoCurrent },dataDevice.rtData.sampleTime,dataDevice.rtData.current))
+      setGraficoPower(addGraficoData({ ...graficoPower },dataDevice.rtData.sampleTime,dataDevice.rtData.power))
 
     }
   }, [monitorData])
 
+  /* Filtro de informacion en grafico */
   const Filtro = (dias) => {
 
     if (deviceId > 0) {
@@ -93,35 +100,29 @@ const DeviceGraphs = (props) => {
 
         device = res.data.find(data => data.id == deviceId)
 
-        let labelGraph = []
-        let dataGraph = []
+        let labelGraphVoltaje = []
+        let labelGraphCurrent = []
+        let labelGraphPower = []
+
+        let dataGraphVoltaje = []
+        let dataGraphCurrent = []
+        let dataGraphPower = []
 
         device.rtData?.map((obj) => {
           if (new Date(obj.sampleTime) > date) {
-            labelGraph.push(convertISODateToTimeFormat(obj.sampleTime))
+            labelGraphVoltaje.push(convertISODateToTimeFormat(obj.sampleTime))
+            labelGraphCurrent.push(convertISODateToTimeFormat(obj.sampleTime))
+            labelGraphPower.push(convertISODateToTimeFormat(obj.sampleTime))
+
+            dataGraphVoltaje.push(obj.voltaje)
+            dataGraphCurrent.push(obj.current)
+            dataGraphPower.push(obj.power)
           }
         })
 
-        device.rtData?.map((obj) => {
-          if (new Date(obj.sampleTime) > date) {
-            dataGraph.push(obj.voltaje)
-          }
-        })
-
-        setGrafico({
-          labels: labelGraph,
-          datasets: [
-            {
-              label: 'Voltaje',
-              fill: false,
-              lineTension: 0.5,
-              backgroundColor: 'rgba(75,192,192,1)',
-              borderColor: 'rgba(0,0,0,1)',
-              borderWidth: 2,
-              data: dataGraph
-            }
-          ]
-        })
+        setGraficoVoltaje(setGraficoData({ ...graficoVoltaje },labelGraphVoltaje,dataGraphVoltaje))
+        setGraficoCurrent(setGraficoData({ ...graficoCurrent },dataGraphCurrent,dataGraphCurrent))
+        setGraficoPower(setGraficoData({ ...graficoPower },dataGraphPower,dataGraphPower))
       })
     }
   }
@@ -136,7 +137,7 @@ const DeviceGraphs = (props) => {
           <Button variant="primary" type="button" id='ano' onClick={() => Filtro(365)}>Año</Button>
           <h5>Gráfico de Voltaje</h5>
           <Line
-            data={grafico}
+            data={graficoVoltaje}
             options={{
               title: {
                 display: true,
@@ -158,8 +159,54 @@ const DeviceGraphs = (props) => {
 
             }}
           />
-          <h5>Grafico de Voltaje</h5>
-          <h5>Grafico de Voltaje</h5>
+          <h5>Grafico de Power</h5>
+          <Line
+            data={graficoPower}
+            options={{
+              title: {
+                display: true,
+                text: 'Power',
+                fontSize: 20
+              },
+              legend: {
+                display: true,
+                position: 'right'
+              },
+              plugins: {
+                zoom: {
+                  limits: {
+                    y: { min: 0, max: 100 },
+                    y2: { min: -5, max: 5 }
+                  },
+                }
+              }
+
+            }}
+          />
+          <h5>Grafico de Current</h5>
+          <Line
+            data={graficoCurrent}
+            options={{
+              title: {
+                display: true,
+                text: 'Current',
+                fontSize: 20
+              },
+              legend: {
+                display: true,
+                position: 'right'
+              },
+              plugins: {
+                zoom: {
+                  limits: {
+                    y: { min: 0, max: 100 },
+                    y2: { min: -5, max: 5 }
+                  },
+                }
+              }
+
+            }}
+          />          
         </div>
       )}
     </>
--- frontend/src/lib/Utils.js
+++ frontend/src/lib/Utils.js
@@ -11,3 +11,32 @@ export function convertISODateToTimeFormat(ISODate) {
     // console.log(dateTemplate)
     return dateTemplate;
   }
+  export function setGraficoData(state,labelArray,dataArray){
+    state.labels = labelArray
+    state.datasets[0].data = dataArray
+    return state;
+  }
+
+  export function addGraficoData(state,labelObject,dataObject){
+    state.labels.push(labelObject)
+    state.datasets[0].data.push(dataObject)
+    return state;
+  }
+  
+  export function initConfig(backgroundColor,borderColor, label) {
+    let objectConfigGraph = {
+      labels: [],
+      datasets: [
+        {
+          label: label,
+          fill: false,
+          lineTension: 0.5,
+          backgroundColor: backgroundColor,
+          borderColor: borderColor,
+          borderWidth: 2,
+          data: []
+        }
+      ]
+    }
+    return objectConfigGraph
+  }
