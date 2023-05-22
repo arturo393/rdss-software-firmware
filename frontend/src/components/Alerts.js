7d26834fc8de8b1787dfa3305d902877465baeec
Yery Tannus <ytannus@itaum.com>
2023-05-22 17:15:13 -0400

    +configuracion de atenuación de cada dispositivo en página de alertas

--- frontend/src/components/Alerts.js
+++ frontend/src/components/Alerts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Container, Card, Table } from "react-bootstrap"
 import { setMonitorDataEvent } from "../redux/actions/main"
+import axios from "axios"
 import { connect } from "react-redux"
 import green from "../images/green.svg"
 import red from "../images/red.svg"
@@ -12,13 +13,16 @@ let a_current
 let a_agcup
 let a_agcdown
 let a_ptx
+let a_smartTune
+let a_reverse
+let a_attenuation
 
 const Alerts = (props) => {
   const [alerts, setAlerts] = useState([])
+  const [deviceData, setDeviceData] = useState({})
   const { monitorData } = props
   useEffect(() => {
     let currentAlerts = []
-
     monitorData?.map((monitor) => {
       const data = JSON.parse(monitor)
 
@@ -48,12 +52,60 @@ const Alerts = (props) => {
         gupl: a_agcup,
         guwl: a_agcdown,
         power: a_ptx,
+        smartTune: data.smartTune || "no data",
+        reverse: data.reverse || "no data",
+        attenuation: data.attenuation
       }
       currentAlerts.push(device)
     })
     setAlerts(currentAlerts)
   }, [monitorData])
 
+  const handleChange = (e) => {
+    e.preventDefault()
+    setDeviceData({
+      [e.target.id]: e.target.value,
+    })
+  }
+  const saveDevice = (e) => {
+    e.preventDefault()
+    let new_attenuation = e.target.attenuation.value
+    if (new_attenuation < 0 || new_attenuation > 32) {
+      let resultado = document.getElementById(e.target.id.value+"status")
+      resultado.style.display = "block"
+      resultado.innerHTML = "Value should be between 0 and 32"
+      return false
+    }
+
+    axios.get(process.env.NEXT_PUBLIC_APIPROTO + "://" + process.env.NEXT_PUBLIC_APIHOST + ":" + process.env.NEXT_PUBLIC_APIPORT + "/api/device/" + e.target.id.value)
+                      .then(
+                        (result) => {
+                          return result.data[0]
+                        },
+                        (error) => {
+                          console.log(error)
+                        }
+                      )
+                      .then((device) => {
+                        device.attenuation = new_attenuation  * 1 // ensure integer
+                        let status = document.getElementById(e.target.id.value+"status")
+                        let message
+                        axios.post(process.env.NEXT_PUBLIC_APIPROTO + "://" + process.env.NEXT_PUBLIC_APIHOST + ":" + process.env.NEXT_PUBLIC_APIPORT + "/api/device/save", device).then(
+                          (result) => {
+                            console.log(result)
+                            message = "Device updated successfully"
+                          },
+                          (error) => {
+                            message = "Error when try to save device data"
+                            console.log(error)
+                          }
+                        ).then(() => {
+                          status.style.display = "block"
+                          status.innerHTML = message
+                        })
+                      })
+
+  }
   return (
     <>
       <h5 className="text-center">Devices Status</h5>
@@ -84,6 +136,16 @@ const Alerts = (props) => {
                 <th>
                   <h6>Downlink Power</h6>
                 </th>
+                <th>
+                  <h6>Smart Tune</h6>
+                </th>
+                <th>
+                  <h6>Reverse</h6>
+                </th>
+                <th>
+                  <h6>Downlink Attenuation</h6>
+                </th>
+                
               </tr>
             </thead>
             <tbody>
@@ -108,6 +170,27 @@ const Alerts = (props) => {
                     </td>
                     <td>
                       <img alt="" src={data.power} width={20} height={20} />
+                    </td>
+                    <td>
+                      {data.smartTune}
+                    </td>
+                    <td>
+                      {data.reverse}
+                    </td>
+                    <td>
+                      <form onSubmit={saveDevice}>
+                        <div className="input-group mb-3">
+                          <input type="number" className="form-control" id="attenuation" value={data?.attenuation} onChange={handleChange} />
+                          <button className="btn btn-primary" type="submit">Save</button>
+                          <input type="hidden" name="id" id="id" value={data.id}/>
+                        </div>
+                        <div className="row">
+                          <div className="col-md-12 text-center">
+                            <div className="alert alert-info hidden" role="alert" id={data.id+"status"}>OK
+                            </div>
+                          </div>
+                        </div>
+                      </form>
                     </td>
                   </tr>
                 )
