50d3f8a653ed03d0a73a268ad7cbd7988cdc8efd
arturo33 <arturo@sigma-telecom.com>
2024-04-08 09:18:50 -0400

    [ADD] filter by visible

--- frontend/src/components/Rtdata.js
+++ frontend/src/components/Rtdata.js
@@ -28,8 +28,8 @@ const Rtdata = (props) => {
   // const [devices, setDevices] = useState([])
   const [device, setDevice] = useState(activeDeviceId || 0)
   const [deviceData, setDeviceData] = useState({})
+  const [dev_data, setDev] = useState(null);
   const [deviceName, setDeviceName] = useState("=== Select a Device ===")
-
 
   const [dateFrom, handleDateFromChange] = useState(new Date(Date.now() - 3600 * 1000 * 6))
   const [dateTo, handleDateToChange] = useState(new Date())
@@ -37,17 +37,17 @@ const Rtdata = (props) => {
 
 
   useEffect(() => {
-    activeDeviceId?setDevice(activeDeviceId):setDevice(1)
+    activeDeviceId ? setDevice(activeDeviceId) : setDevice(1)
     hiddeSpinner()
   }, [])
-  
+
   useEffect(() => {
     activeDeviceId && setDevice(activeDeviceId)
     handleDateFromChange(new Date(Date.now() - 3600 * 1000 * 6))
     handleDateToChange(new Date())
 
   }, [activeDeviceId])
-  
+
   useEffect(() => {
     // console.log("fields.length",fields.length)
     // console.log("device",device)
@@ -55,14 +55,16 @@ const Rtdata = (props) => {
       getDeviceRTData(device)
       document.getElementById("device").value = device
       const dev = devices.find((d) => Number(d.id) === Number(device))
-      setDeviceName(dev.name ? dev.name + "(" + dev.type + "-" + dev.id + ")" : dev.type + "-" + dev.id) 
+      console.log("device", dev)
+      setDev(dev)
+      setDeviceName(dev.name ? dev.name + "(" + dev.type + "-" + dev.id + ")" : dev.type + "-" + dev.id)
     }
-         
+
   }, [device])
-  
+
 
   const getDeviceRTData = async (device = 0) => {
-    if (device > 0) { 
+    if (device > 0) {
       showSpinner()
       console.log("Getting device data, ID=" + device)
       let x = []
@@ -82,17 +84,17 @@ const Rtdata = (props) => {
         const datetime = getDateTime(data?._id)
         x.push(datetime)
         Object.keys(data?.field_values).map((key) => {
-          
+
           rtd[key]?.push(data?.field_values[key].value)
-          marker.color.push(data?.field_values[key].alert?"red":"lightblue")
+          marker.color.push(data?.field_values[key].alert ? "red" : "lightblue")
           let text_content = `<b>Device: ${data._id.device}</b><br>\n`
-          text_content = data?.connected?"":"Disconnect"
-          text_content += data?.field_values[key].alert?fields.find(f => key === f._id).name+" Out of Limits":"no alerts"
+          text_content = data?.connected ? "" : "Disconnect"
+          text_content += data?.field_values[key].alert ? fields.find(f => key === f._id).name + " Out of Limits" : "no alerts"
           text.push(text_content)
         })
       })
 
-    
+
 
       setRtData({ x, rtd, marker, text })
       hiddeSpinner()
@@ -100,7 +102,7 @@ const Rtdata = (props) => {
 
 
   }
-  
+
 
   const getDateTime = (obj) => {
     let dateString = obj.year
@@ -126,13 +128,12 @@ const Rtdata = (props) => {
     showSpinner()
     e.preventDefault()
     const deviceSelector = document.getElementById("device")
-    if (deviceSelector.value >= 0) {
-      setDevice(deviceSelector.selectedIndex+1)
-      const dev = devices.find((d) => Number(d.id) === Number(deviceSelector.value+1))
+    if (deviceSelector.value > 0) {
+      setDevice(deviceSelector.selectedIndex)
+      const dev = devices.find((d) => Number(d.id) === Number(deviceSelector.value))
       setDeviceName(dev.name ? dev.name + "(" + dev.type + "-" + dev.id + ")" : dev.type + "-" + dev.id)
-      setDeviceData(devices.find((d) => d.id === deviceSelector.selectedIndex+1))
+      setDeviceData(devices.find((d) => d.id === deviceSelector.selectedIndex))
     }
-    hiddeSpinner()
   }
 
   const hiddeSpinner = () => {
@@ -154,78 +155,102 @@ const Rtdata = (props) => {
 
   return (
     <>
-    <h5 className="text-center w-100 sigmaRed text-light">RT-Data {device > 0 && deviceName}</h5>
-    
-   
-    <div className="containers text-center">
-      <span className="spinnerContainer" id="spinnerContainer">
-        <span>Loading...</span>
-        <div className="spinner-border text-light" role="status" name="spinner" id="spinner" style={{ maxWidth: "50px", maxHeight: "50px" }}></div>
-      </span>
+      <h5 className="text-center w-100 sigmaRed text-light">RT-Data {device > 0 && deviceName}</h5>
 
-      <div className="card h-100">
-        <div className="card-body text-center">
-          <div className="input-group mb-3">
 
-          <span className="input-group-text" id="device-label">
-              Device
-            </span>
-            <select className="form-select" id="device" onChange={(e) => setDevice(e.target.value)} value={device}>
-              {/* <option value={0}>{deviceName}</option> */}
-              {devices.map((d) => {
-                return (
-                  <option value={d.id}>
-                    {d.name} ({d.type}-{d.id})
-                  </option>
-                )
+      <div className="containers text-center">
+        <span className="spinnerContainer" id="spinnerContainer">
+          <span>Loading...</span>
+          <div className="spinner-border text-light" role="status" name="spinner" id="spinner" style={{ maxWidth: "50px", maxHeight: "50px" }}></div>
+        </span>
+
+        <div className="card h-100">
+          <div className="card-body text-center">
+            <div className="input-group mb-3">
+
+              <span className="input-group-text" id="device-label">
+                Device
+              </span>
+              <select className="form-select" id="device" onChange={(e) => setDevice(e.target.value)} value={device}>
+                {/* <option value={0}>{deviceName}</option> */}
+                {devices.map((d) => {
+                  return (
+                    <option value={d.id}>
+                      {d.name} ({d.type}-{d.id})
+                    </option>
+                  )
+                })}
+              </select>
+
+              <ThemeProvider theme={theme}>
+                <MuiPickersUtilsProvider utils={LuxonUtils}>
+                  {/* <div class="input-group mb-3"> */}
+                  <span class="input-group-text" id="datetime1">
+                    From:
+                  </span>
+                  <DateTimePicker class="form-control" aria-label="dateFrom" aria-describedby="datetime1" variant="inline" value={dateFrom} onChange={handleDateFromChange} />
+
+                  <span class="input-group-text" id="datetime2">
+                    To:
+                  </span>
+                  <DateTimePicker class="form-control" aria-label="dateTo" aria-describedby="datetime2" variant="inline" value={dateTo} onChange={handleDateToChange} />
+                  {/* </div> */}
+                </MuiPickersUtilsProvider>
+              </ThemeProvider>
+
+
+
+              <button id="searchDevice" onClick={handleSearch} className="btn btn-primary">
+                Search
+              </button>
+            </div>
+            <div className="text-center mt-2 mb-2">
+
+            </div>
+            {console.log("dev.field_values ", dev_data)}
+            {
+              dev_data?.fields_values &&
+              Object.entries(dev_data?.fields_values).map(([fieldId, fieldValue]) => {
+                const field = fields.find((f) => f._id === fieldId);
+                const thisDevice = devices.find(d => d.id === dev_data.id);
+                const fieldDef = thisDevice?.fields_values && field?._id && thisDevice?.fields_values[field._id] || null;
+
+                if (field?.plottable && fieldDef?.visible && rtData.x?.length > 0) {
+                  console.log("fieldDef", fieldDef)
+                  console.log("field", field)
+                  console.log("fieldValue", fieldValue)
+
+                  return (
+                    <div>
+                      {fieldValue?.field_name?
+                      (<Chart className="w-100 bg-danger" deviceId={device} rtData={rtData} label={fieldValue?.field_name} filter={field._id} color={field.color || "lightblue"} />)
+                      :(<Chart className="w-100 bg-danger" deviceId={device} rtData={rtData} label={field.name} filter={field._id} color={field.color || "lightblue"} />)}
+                    </div>
+                  );
+                }
+
               })}
-            </select>
 
-            <ThemeProvider theme={theme}>
-              <MuiPickersUtilsProvider utils={LuxonUtils}>
-                {/* <div class="input-group mb-3"> */}
-                <span class="input-group-text" id="datetime1">
-                  From:
-                </span>
-                <DateTimePicker class="form-control" aria-label="dateFrom" aria-describedby="datetime1" variant="inline" value={dateFrom} onChange={handleDateFromChange} />
+            {(device > 0 && fields && rtData.x?.length > 0 && false) ? (
+              <>
 
-                <span class="input-group-text" id="datetime2">
-                  To:
-                </span>
-                <DateTimePicker class="form-control" aria-label="dateTo" aria-describedby="datetime2" variant="inline" value={dateTo} onChange={handleDateToChange} />
-                {/* </div> */}
-              </MuiPickersUtilsProvider>
-            </ThemeProvider>
-
-            
-
-            <button id="searchDevice" onClick={handleSearch} className="btn btn-primary">
-              Search
-            </button>
-          </div>
-          <div className="text-center mt-2 mb-2">
-                
-              </div>
-          {(device > 0 && fields &&  rtData.x?.length > 0)? (
-            <>
-              
-              {/* {console.log("RTDATA", rtData)} */}
-              {
+                {/* {console.log("RTDATA", rtData)} */}
+                {
                   fields.filter(field => field.plottable).map(field => (
                     // rtData = { x, rtd, marker, text }
                     <div>
-                    <Chart className="w-100 bg-danger" deviceId={device}  rtData={rtData} label={field.name} filter={field._id} color={field.color || "lightblue"} />
+                      <Chart className="w-100 bg-danger" deviceId={device} rtData={rtData} label={field.name} filter={field._id} color={field.color || "lightblue"} />
                     </div>
                   ))
-              }
-              
-            </>
-          ):(
-            <div>No Data Found</div>
-          )}
+                }
+
+              </>
+            ) : (
+              <div>No Data Found</div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
